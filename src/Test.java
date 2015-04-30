814e81310a8e7b0bb87c0789160261f4b307d605
kyjeong <kyjeong@ebay.com>
2015-04-30 09:48:28 +0900

    Revert "dev002_cmp"
    This reverts commit 951cf8a76382a830c70bdaf684e305bb02abfa11.
    
    Conflicts:
    	src/Test.java

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 		System.out.println("test");
 		System.out.println("test2");
 		
-		System.out.println("dev002_cmd modify.");
+		System.out.println("dev001 에서 추가했다.");
 		System.out.println("dev002 add.");
 		System.out.println("dev002 add 2.");
 		System.out.println("dev002 add 3.");
