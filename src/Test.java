ad91fb944dee10bd9a6082cb278cac1c888f65cd
appolo13 <appolo13@gmail.com>
2015-04-30 09:14:11 +0900

    add4

--- src/Test.java
+++ src/Test.java
@@ -4,5 +4,9 @@ public class Test {
 		System.out.println("test2");
 		
 		System.out.println("dev002_cmd modify.");
+		System.out.println("dev002 add.");
+		System.out.println("dev002 add 2.");
+		System.out.println("dev002 add 3.");
+		System.out.println("dev002 add 4.");
 	}
 }
